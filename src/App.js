28eed43c80d8351d2bd77726fc537ece0543ee43
Sam <super8989@gmail.com>
2020-11-09 13:46:22 -0500

    Created states for the inputs

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import Logo from './components/Logo';
 import Header from './components/Header';
 import Email from './components/Email';
@@ -8,6 +10,10 @@ import Footer from './components/Footer';
 import './App.scss';
 
 function App() {
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const [confirmPassword, setConfirmPassword] = useState('');
+
 	return (
 		<div className='app'>
 			<div className='app--container'>
